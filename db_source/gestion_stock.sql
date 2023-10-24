f1638dd26dab114162711bf353cf769604c179dc
anas-er25 <anaserrakibi@gmail.com>
2023-10-24 23:15:09 +0100

    la page de login

--- db_source/gestion_stock.sql
+++ db_source/gestion_stock.sql
@@ -1,51 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Oct 24, 2023 at 05:48 PM
--- Server version: 8.0.34-0ubuntu0.20.04.1
--- PHP Version: 8.2.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `gestion_stock`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Categories`
---
-
 CREATE TABLE `Categories` (
   `CategoryID` int NOT NULL,
   `CategoryName` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `Categories`
---
+);
 
 INSERT INTO `Categories` (`CategoryID`, `CategoryName`) VALUES
 (1, 'Electronics'),
 (2, 'Clothing'),
 (3, 'Home Appliances');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Products`
---
 
 CREATE TABLE `Products` (
   `ProductID` int NOT NULL,
@@ -54,11 +20,9 @@ CREATE TABLE `Products` (
   `Price` decimal(10,2) DEFAULT NULL,
   `CategoryID` int DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
---
--- Dumping data for table `Products`
---
+
 
 INSERT INTO `Products` (`ProductID`, `Name`, `Description`, `Price`, `CategoryID`, `image`) VALUES
 (1, 'Smartphone', 'High-end smartphone with great features', 599.99, 1, 'product1.jpg'),
@@ -66,55 +30,38 @@ INSERT INTO `Products` (`ProductID`, `Name`, `Description`, `Price`, `CategoryID
 (3, 'T-shirt', 'Cotton T-shirt, white, size M', 19.99, 2, 'product3.jpg'),
 (4, 'Refrigerator', 'Energy-efficient refrigerator', 899.99, 3, 'product4.jpg');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `Roles`
---
 
 CREATE TABLE `Roles` (
   `RoleID` int NOT NULL,
   `RoleName` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
---
--- Dumping data for table `Roles`
---
+
 
 INSERT INTO `Roles` (`RoleID`, `RoleName`) VALUES
 (1, 'Admin'),
 (2, 'Manager'),
 (3, 'Employee');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `Stock`
---
 
 CREATE TABLE `Stock` (
   `StockID` int NOT NULL,
   `ProductID` int DEFAULT NULL,
   `Quantity` int DEFAULT NULL,
   `StockStatus` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Users`
---
 
 CREATE TABLE `Users` (
   `UserID` int NOT NULL,
   `Username` varchar(30) DEFAULT NULL,
   `Password` varchar(255) DEFAULT NULL,
   `RoleID` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
---
--- Dumping data for table `Users`
---
+
 
 INSERT INTO `Users` (`UserID`, `Username`, `Password`, `RoleID`) VALUES
 (1, 'Ahmed', 'password123', 1),
@@ -122,69 +69,37 @@ INSERT INTO `Users` (`UserID`, `Username`, `Password`, `RoleID`) VALUES
 (3, 'Khaled', 'strongpwd', 3),
 (4, 'Noura', 'mysecretpw', 3);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `Categories`
---
 ALTER TABLE `Categories`
   ADD PRIMARY KEY (`CategoryID`);
 
---
--- Indexes for table `Products`
---
+
 ALTER TABLE `Products`
   ADD PRIMARY KEY (`ProductID`),
   ADD KEY `CategoryID` (`CategoryID`);
 
---
--- Indexes for table `Roles`
---
+
 ALTER TABLE `Roles`
   ADD PRIMARY KEY (`RoleID`);
 
---
--- Indexes for table `Stock`
---
+
 ALTER TABLE `Stock`
   ADD PRIMARY KEY (`StockID`);
 
---
--- Indexes for table `Users`
---
+
 ALTER TABLE `Users`
   ADD PRIMARY KEY (`UserID`),
   ADD KEY `RoleID` (`RoleID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `Users`
---
 ALTER TABLE `Users`
   MODIFY `UserID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `Products`
---
 ALTER TABLE `Products`
   ADD CONSTRAINT `Products_ibfk_1` FOREIGN KEY (`CategoryID`) REFERENCES `Categories` (`CategoryID`);
 
---
--- Constraints for table `Users`
---
+
 ALTER TABLE `Users`
   ADD CONSTRAINT `Users_ibfk_1` FOREIGN KEY (`RoleID`) REFERENCES `Roles` (`RoleID`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
