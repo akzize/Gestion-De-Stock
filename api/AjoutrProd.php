f17d5fac2791467fdb76db3b414d0be0f0f753be
Abderrahim-belaid-123 <AbdeBelaid218@gmail.com>
2023-10-25 00:07:21 +0100

    ajout des photos dans la table produit

--- api/AjoutrProd.php
+++ api/AjoutrProd.php
@@ -1,30 +1,56 @@
 <?php
 // Fait appel au fichier de connexion à la base de données
-include_once __DIR__."/../config/connect.php";
+include_once __DIR__ . "/../config/connect.php";
 
 // S'assurer que le button ajouter est deja cliquer
-$action=$_POST["action"];
+$action = $_POST["action"];
 
 // Récupère les info  de catégorie en utilisant la variable superglobale POST
 $nomCategorie = $_POST["nameCategorie"];
 
-$idCategorie=$_POST['idCategorie'];
+$idCategorie = $_POST['idCategorie'];
 
-if (isset($action) && $action=="Ajouter"){
+if (isset($action) && $action == "Ajouter") {
 
-if (isset($nomCategorie)){
+    if (isset($nomCategorie)) {
 
-    // Prépare la requête SQL avec un paramètre à lier
-$stmt = $conn->prepare("INSERT INTO Categories (CategoryID,CategoryName) VALUES(?,?)");
+        // Prépare la requête SQL avec un paramètre à lier
+        $stmt = $conn->prepare("INSERT INTO Categories (CategoryID,CategoryName) VALUES(?,?)");
 
-// Lie la valeur de $nomCategorie au paramètre de la requête
+        // Lie la valeur de $nomCategorie au paramètre de la requête
 
-$stmt->bindParam(1,$idCategorie); // il doit supprimer ce param si le champ idCategorie autoIncrementer
-$stmt->bindParam(2,$nomCategorie);
-
-}
+        $stmt->bindParam(1, $idCategorie); // il doit supprimer ce param si le champ idCategorie autoIncrementer
+        $stmt->bindParam(2, $nomCategorie);
 
 
+        if ($stmt->execute()) {
+            echo "la categorie est insérée avec succès.";
+        } else {
+            echo "Erreur lors de l'insertion de la catégorie : " . $stmt->errorInfo()[2];
+        }
+    } else {
+        // Récupère les info  de produit en utilisant la variable superglobale POST
+
+        $nomProduit = $_GET['nomProduit'];
+        $idProduit = $_GET['idProduit'];
+        $descProduit = $_GET['descProduit'];
+        $prixProduit = $_GET['prixProduit'];
+        $imageProduit = $_GET['imageProduit'];
+
+        // Prépare la requête SQL avec un paramètre à lier
+        $stmt = $conn->prepare("INSERT INTO Products (`ProductID`,`Name`,`Description`,Price,CategoryID,`image`) VALUES(?,?,?,?,?,?)");
+        $stmt->bindParam(1, $idProduit);
+        $stmt->bindParam(2, $nomProduit);
+        $stmt->bindParam(3, $descProduit);
+        $stmt->bindParam(4, $prixProduit);
+        $stmt->bindParam(5, $idCategorie);
+
+        if ($stmt->execute()) {
+            echo "le produit est insérée avec succès.";
+        } else {
+            echo "Erreur lors de l'insertion du produit : " . $stmt->errorInfo()[2];
+        }
+    }
 }
 
 
@@ -49,24 +75,40 @@ $stmt->bindParam(2,$nomCategorie);
 // $stmt->bindParam(2,$nomCategorie);
 
 // Récupère les info  de produit en utilisant la variable superglobale POST
-$nomProduit=$_GET['nomProduit'];
-$idProduit=$_GET['idProduit'];
-$descProduit=$_GET['descProduit'];
-$prixProduit=$_GET['prixProduit'];
-$imageProduit=$_GET['imageProduit'];
+$nomProduit = $_GET['nomProduit'];
+$idProduit = $_GET['idProduit'];
+$descProduit = $_GET['descProduit'];
+$prixProduit = $_GET['prixProduit'];
+$imageProduit = $_GET['imageProduit'];
 // $nomProduit=$_FILES['imageProduit']['name'];
 // $nomProduit=$_FILES['imageProduit']['tempnaame'];
 
 $stmt1 = $conn->prepare("INSERT INTO Products (`ProductID`,`Name`,`Description`,Price,CategoryID,`image`) VALUES(?,?,?,?,?,?)");
 
+
+
+// le code pour placer l'image dans le dossier imags
+
+$uploads_dir = '/images';
+
+$tmp_name = $_FILES["imageProduit"]["tmp_name"];
+$nameImage = $_FILES["imageProduit"]["name"];
+move_uploaded_file($tmp_name, "$uploads_dir/$nameImage");
+
+
+
 // Lie la valeur de $nomCategorie au paramètre de la requête
 
-$stmt1->bindParam(1,$idProduit);
-$stmt1->bindParam(2,$nomProduit);
-$stmt1->bindParam(3,$descProduit);
-$stmt1->bindParam(4,$prixProduit);
-$stmt1->bindParam(5,$idCategorie);
-$stmt1->bindParam(6,$imageProduit);
+$stmt1->bindParam(1, $idProduit);
+$stmt1->bindParam(2, $nomProduit);
+$stmt1->bindParam(3, $descProduit);
+$stmt1->bindParam(4, $prixProduit);
+$stmt1->bindParam(5, $idCategorie);
+$stmt1->bindParam(6, $nameImage);
+
+
+
+
 
 
 if ($stmt1->execute()) {
@@ -98,5 +140,3 @@ if ($stmt1->execute()) {
 // } else {
 //     echo "Erreur lors de l'insertion de la catégorie : " . $stmt->errorInfo()[2];
 // }
-
-?>
