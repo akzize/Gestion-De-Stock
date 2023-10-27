8603f87a0caa34c77ad5667a527556cf3645d298
Abderrahim-belaid-123 <AbdeBelaid218@gmail.com>
2023-10-27 22:44:16 +0100

    j'ai ajouter les api supprimer (produits,catgorie)

--- api/supprimerProd.php
+++ api/supprimerProd.php
@@ -1,4 +1,19 @@
 <?php
-echo "supprimer api";
+// Fait appel au fichier de connexion à la base de données
+include_once __DIR__ . "/../config/connect.php";
+
+if (isset($_POST["numeroProduit"])){
+    $numeroProduit = $_POST["numeroProduit"];
+    $stmt = $conn->prepare("DELETE FROM Products WHERE ProductID = :nomProduit");
+  // Ajoutez des % autour de la valeur de recherche
+    $stmt->bindParam(':nomProduit', $numeroProduit);
+    $res=$stmt->execute();
+    if ($res){
+        echo json_encode("le produit est supprimer avec succes");
+    }
+
+    
+}
+
 
 ?>
